f8bdfe98250ad5c08cd9d182084c688e5c55aa79
Alexander Weigl <spamling@web.de>
2012-12-15 05:14:58 +0100

    changes for travis ci

---
+++ test/edu/kit/tm/afsp/g1/TestMain.java
@@ -0,0 +1,8 @@
+package edu.kit.tm.afsp.g1;
+
+public class TestMain {
+    public static void main(String[] args) {
+	org.junit.runner.JUnitCore.main("edu.kit.tm.afsp.g1.HeaderTest",
+		"edu.kit.tm.afsp.g1.LocalFileList");
+    }
+}
